0d64bbcf62bb9efdd8383bec81d1ef437c2bfbd7
kdh949 <kdh949@naver.com>
2024-04-11 13:45:57 +0900

    자전거+인간에너지 조합으로 수행률 100% 달성

--- src/main/java/kr/easw/lesson04/ModularExample.java
+++ src/main/java/kr/easw/lesson04/ModularExample.java
@@ -13,6 +13,9 @@ package kr.easw.lesson04;
  * <p>
  * 문제가 너무 어려운 경우, 기본 조건만 충족해도 괜찮습니다.
  */
+
+
+
 public class ModularExample {
     public static int MAX_TICK = 5000;
 
@@ -58,7 +61,7 @@ public class ModularExample {
      * 상속하여 구현한 Vehicle 객체를 반환해야 합니다.
      */
     public static Vehicle getVehicle() {
-        throw new RuntimeException("이 코드 라인을 지우고, 이곳에서 작성하십시오.");
+        return new Vehicle.MyVehicle();
     }
 
     // 해당 클래스를 상속하여 구현하여야 합니다.
@@ -74,6 +77,23 @@ public class ModularExample {
         // 각 틱이 실행되기 전에 실행됩니다.
         // 이 메서드를 통해 조금 더 나은 결과를 도출할 수 있습니다.
         public abstract void onTick(int currentTick, int fuel);
+
+        static class MyVehicle extends Vehicle {
+            @Override
+            public Energy getEnergy() {
+                return new HumanEnergy();
+            }
+
+            @Override
+            public VehicleType getType() {
+                return new Bike();
+            }
+
+            @Override
+            public void onTick(int currentTick, int fuel) {
+                // Implement this method based on your specific requirements
+            }
+        }
     }
 
 
